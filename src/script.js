e60e322aa6d81131c4d21d874e36f69498cef00c
Devdav3 <davidodunlami12@gmail.com>
2022-02-22 21:37:54 -0500

    lab 2 update

--- src/script.js
+++ src/script.js
@@ -5,13 +5,11 @@
 const randomDamage = () => {
   return Math.floor(Math.random() * 10 + 1);
 }
-// console.log(randomDamage());
 
 const chooseOption = (opt1, opt2) => {
   let randNum = Math.floor(Math.random() * 2);
   return randNum === 0 ? opt1 : opt2;
 }
-// console.log(chooseOption); mare sure this is right later bullet 2
 
 const attackPlayer = function (health) {
 return health = health - randomDamage(); 
@@ -28,25 +26,32 @@ const logDeath = (winner, loser) => {
 
 const isDead = (health) => {
   // return health <= 0 
-  if (health <= 0) {return true} else {return false}
+  if (health <= 0) {
+    return true; 
+  }else {
+    return false;
+  }
 }
 
-function fight ( player1, player2, player1Health, player2Health) {
-
+function fight(player1, player2, player1Health, player2Health) {
   while (true) {
-    let attacker = chooseOption (player1,player2)
+    let attacker = chooseOption (player1, player2);
+
+    if (attacker === player1) {
+      player2Health = attackPlayer(player2Health);
+      logHealth(player2, player2Health);
+
+      if (isDead(player2Health) === true) {
+        logDeath(player1, player2);
+        break;
+      }
+    } else {
+      player1Health = attackPlayer(player1Health);
+      logHealth(player1, player1Health);
+      if (isDead(player1Health)) {
+        logDeath(player2, player1);
+        break;
+      }
+    }
   }
-if (attacker === player1){
-  player2Health = attackPlayer (player2Health)
-  logHealth (player2, player2Health)
-} 
-if (isDead(player2Health)){
-  logDeath(player1,player2)
-  break;
-} else {
-  player1Health = attackPlayer (player1Health)
-  logHealth (player1, player1Health)
-} if (isDead(player1Health)){
-  logDeath(player2,player1)
-  break;
 }
